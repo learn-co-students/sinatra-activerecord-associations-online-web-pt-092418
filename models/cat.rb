420f021c3f8bd26e40dda49a9299fae973bb0de9
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-02-22 00:54:34 -0500

    Done.

--- models/cat.rb
+++ models/cat.rb
@@ -1,3 +1,7 @@
 class Cat < ActiveRecord::Base
   belongs_to :owner
+
+Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold")
+Cat.create(:name => "Hannah", :age => 2, :breed => "Tabby")
+Cat.create(:name => "Patches", :age => 2, :breed => "Calico")
 end
